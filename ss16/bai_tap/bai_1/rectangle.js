e321e7cf7538fb813478e783a6824e799f31ef81
hungnguyen988 <hungnguuen988@gmail.com>
2024-08-26 09:19:37 +0700

    =nop_bai

--- ss16/bai_tap/bai_1/rectangle.js
+++ ss16/bai_tap/bai_1/rectangle.js
@@ -23,19 +23,19 @@ class Rectangle {
         let perimeter = 2*(this.width+this.heigth);
         return perimeter;
     }
-    draw(context) {
-        context.clearRect(0, 0,500,500);
-        context.fillStyle = "blue";
-        context.fillRect(20,20 , this.width, this.heigth);
+    draw(canvas) {
+        canvas.clearRect(0, 0,500,500);
+        canvas.fillStyle = "blue";
+        canvas.fillRect(20,20 , this.width, this.heigth);
     }
 }
-let rectangle1 = new Rectangle(200,100);
+let rectangle1 = new Rectangle(100,50);
 document.getElementById("output1").innerHTML = "diện tích hinh chữ nhật : " + rectangle1.getArea();
 document.getElementById("output2").innerHTML = "chu vi hinh chữ nhật : " + rectangle1.getPerimeter();
 
-let  canvas = document.getElementById('rectangleCanvas');
-let context = canvas.getContext('2d');
-rectangle1.draw(context);
+ let canvas = document.getElementById('rectangleCanvas').getContext('2d');
+
+rectangle1.draw(canvas);
 rectangle1.setHeigth(200);
 rectangle1.setWidth(400);
 document.getElementById("output3").innerHTML = "diện tích hinh chữ nhật sau thay đổi : " + rectangle1.getArea();
